9528c3e30134189c354daabd6660662dca7e5375
Bruno Becoski <brunobecoski@gmail.com>
2022-06-27 23:04:19 -0300

    Responsividade da page Subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,24 +28,24 @@ export function Subscribe() {
   }
 
   return (
-    <>
-      <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-        <img className="max-w-[654px] max-h-[575px] top-[10px] absolute" src="/src/assets/react-icon.png" alt="" />
-        <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto z-10">
-          <div className="max-w-[640px]">
+    <div className="relative overflow-hidden">
+      <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center overflow-hidden">
+        <img className="max-w-[calc(100vw_+_180px)] top-[-30px] md:top-[10px] absolute" src="/src/assets/react-icon.png" alt="" />
+        <div className="w-full max-w-[1440px] flex flex-col md:flex-row items-center justify-around mt-10 md:mt-20 mx-auto z-10">
+          <div className="md:max-w-[640px] px-6 flex md:block flex-col justify-center items-center md:px-0 text-center md:text-left">
             <Logo />
 
-            <h1 className="mt-8 text-[2.5rem] leading-tight">
+            <h1 className="mt-6 md:mt-8 text-3xl md:text-[2.5rem] leading-tight">
               Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
             </h1>
 
-            <p className="mt-4 text-gray-200 leading-relaxed">
+            <p className="mt-6 text-sm md:text-base text-gray-200 leading-relaxed">
               Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
             </p>
           </div>
 
-          <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-            <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+          <div className="w-full md:w-auto mt-8 md:mt-0 py-8 px-6 md:p-8 bg-gray-700 border-t border-b md:border border-gray-500 md:rounded">
+            <strong className="text-[1.125rem] md:text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
             <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
               <input
@@ -72,9 +72,9 @@ export function Subscribe() {
           </div>
         </div>
 
-        <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+        <img src="/src/assets/code-mockup.png" className="mt-4 md:mt-10" alt="" />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
